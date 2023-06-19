187a4f4ab99d264ab12f2dd937859ffd0f6564f8
Korkoe Dumashie <dumashieanthony@gmail.com>
2023-06-19 18:56:32 +0100

    Built html boilerplate

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="signup.css">
+</head>
+
+<body>
+    <!---login form--->
+    <div class="form-container">
+        <form action="" method="POST">
+            <h3>Login now</h3>
+            <input type="email" name="email" placeholder="Enter your Email">
+            <input type="password" id="password" name="password"  placeholder="Enter your password">
+            <!-- <div class="showpassword">
+                <input type="checkbox" id="showPasswordCheckbox">
+                <label for="showPasswordCheckbox">Show password</label>
+            </div> -->
+            <input type="submit" name="login now" value="login now" class="form-btn">
+            <p>don't have an account? <a href="signup.php">signup now</a></p>
+        </form>
+    </div>
+
+    
---
+++ signUp.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>signup</title>
+    <link rel="stylesheet" href="signup.css">
+</head>
+<body>
+    <!--signup form-->
+    <div class="form-container">
+        <form action="signUp.inc.php" method="POST">
+            <h3>Register now</h3>
+            <input type="type" name="name"  placeholder="Enter your name">
+            <input type="email" name="email"  placeholder="Enter your Email">
+            <input type="password" id="password" name="password" required placeholder="Enter your password">
+            <!-- <div class="showpassword">
+                <input type="checkbox" id="showPasswordCheckbox1">
+                <label for="showPasswordCheckbox1">Show password</label>
+            </div> -->
+            <input type="password" name="cpassword" placeholder="Confirm your password">
+            <!-- <div class="showpassword">
+                <input type="checkbox" id="showPasswordCheckbox2">
+                <label for="showPasswordCheckbox2">Show password</label>
+            </div> -->
+            <!-- <span id="passwordValidationMsg" style="color: red;"></span> -->
+            <select name="" name="user_type" >
+                <option value="User">User</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <input type="submit" name="registerNow" value="register now" class="form-btn">
+            <p>already have an account? <a href="login.php">login now</a></p> 
+        </form>
+    </div>
+
+    <!-- <script src="script.js"></script> -->
+    
+</body>
+</html>
